f0a3b7d93916520d4531827768c69b10ac398cf4
gabriel6988 <83712515+gabriel6988@users.noreply.github.com>
2023-10-15 17:08:33 -0300

    Update ContaCorrente.java

--- code/MainCode/ContaCorrente.java
+++ code/MainCode/ContaCorrente.java
@@ -1,64 +1,69 @@
+import java.time.LocalDate;
 /**
-* <h1>ContaInvestimento</h1>
-* Classe Conta Investimento (subclasse de conta).
-* @author Leandro Sena de Andrade Machado
-* @since 07/10/2023
+* <h1>Conta Corrente</h1>
+* Classe Conta Corrente (subclasse de conta).
+* @author Gabriel Alejandro Figueiro Galindo.
+* @since 02/10/2023
 */
-public class ContaInvestimento extends Conta{
-    private double rendimentoDiario;//Rendimento diário (positivo ou negativo).
-    private final double Imposto = 15;//Imposto de 15% sobre o rendimento no saque;
-    private final double Taxa = 1.5;//taxa de 1,5% sobre o rendimento no saque;
+public class ContaCorrente extends Conta{
+    private final double TaxaMensal = 20;//Taxa mensal exclusiva;
+    private double SaqueEspecial;//Saque especial até R$200 além do saldo (8% ao mês de juros);
 
     //Construtor;
-    public ContaInvestimento(int numeroConta, double saldo, double rendimentoDiario){
+    public ContaCorrente(int numeroConta, double saldo){
         super(numeroConta, saldo);
-        this.rendimentoDiario = rendimentoDiario;
+        this.SaqueEspecial = 200.0;
     }
 
     //Getters
-    public double getRendimentoDiario(){
-        return rendimentoDiario;
+    public double getTaxaMensal(){
+        return TaxaMensal;
+    }
+    public double getSaqueEspecial(){
+        return SaqueEspecial;
     }
 
     //Setters
-    public void setRendimentoDiario(double rendimentoDiario){
-        this.rendimentoDiario = rendimentoDiario;
+    public void setSaqueEspecial(double SaqueEspecial){
+        this.SaqueEspecial = SaqueEspecial;
     }
 
-    //Aplicar o rendimento diário;
-    public void aplicarRendimentoDiario(){
-        double saldoAtual = getSaldo();
-        double rendimentoHoje = saldoAtual * (rendimentoDiario / 100.0);
-        double novoSaldo = saldoAtual + rendimentoHoje;
-        setSaldo(novoSaldo);
-        this.adicionarTransacao("Rendimento Diário", rendimentoHoje, this.getSaldo());
+    //Função que cobra a taxa mensal (chamar no começo de cada mês);
+    public void aplicarTaxaMensal(){
+        LocalDate currentDate = LocalDate.now();
+        int currentDay = currentDate.getDayOfMonth();
+        if(currentDay == 1){
+            double saldoAtual = getSaldo();
+            double novoSaldo = saldoAtual - TaxaMensal;
+            setSaldo(novoSaldo);
+            this.adicionarTransacao("Taxa Mensal (Conta Corrente)", -TaxaMensal, this.getSaldo());
+        }
     }
 
-    //Função de saque;
+    //Função de saque (possui um Saque especial até R$200);
     @Override
     public double Sacar(double sacar){
         double saldoAtual = getSaldo();
-        double rendimento = saldoAtual * (rendimentoDiario / 100.0);
-
         if(sacar <= saldoAtual){
-            //Calcula o valor do imposto e taxa sobre o rendimento.
-            double impostoSaque = rendimento * (Imposto / 100.0);
-            double taxaSaque = rendimento * (Taxa / 100.0);
-
-            //Valor do rendimento após o imposto e a taxa;
-            double rendimentoTotal = rendimento - impostoSaque;
-            rendimentoTotal -= taxaSaque;
-
-            //Total removido após o imposto e a taxa do rendimento;
-            double TotalRemovido = Math.round((sacar + impostoSaque + taxaSaque) * 100.0) / 100.0;
-            
-            saldoAtual = Math.round((saldoAtual + rendimentoTotal - TotalRemovido) * 100.0) / 100.0;
+            //O saque é dentro do saldo disponível, sem aplicar juros.
+            saldoAtual -= sacar;
             setSaldo(saldoAtual);
-
-            this.adicionarTransacao("Saque", -TotalRemovido, this.getSaldo());
-            return TotalRemovido;
+            this.adicionarTransacao("Saque", -sacar, this.getSaldo());
+            return sacar;
         }
+        else if(sacar <= saldoAtual + getSaqueEspecial()){
+            //O saque ultrapassa o saldo, aplica juros ao valor excedente.
+            double excedente = sacar - saldoAtual;
+            double juros = excedente * 0.08;//8% de juros;
+            saldoAtual = 0;
+            SaqueEspecial -= excedente + juros;
+            setSaldo(saldoAtual);
+            setSaqueEspecial(SaqueEspecial);
+            this.adicionarTransacao("Saque Especial", -sacar, this.getSaldo());
+            return sacar;
+        } 
         else{
+            //O valor a ser sacado é maior do que o limite permitido.
             throw new IllegalArgumentException("Valor de saque excede o limite disponível.");
         }
     }
